ae9105e827670cafb9bd60c8bdfb78cfee597355
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-03-02 11:39:43 +0530

    deploying bkd v3

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://mini-loan-app-bkd.versal.app"],
+    origin: ["https://mini-loan-webapp.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
