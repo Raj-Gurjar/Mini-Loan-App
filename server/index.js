fe352e14884ee7dbdd4117f6a72b2c2e2bbb946c
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-03-02 11:04:13 +0530

    deploying bkd v1

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("*", cors());
+app.use(cors(
+  {
+    origin: ["https://mini-loan-app-bkd.versal.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 app.use("/api/user", require("./routes/user"));
 app.use("/api/loan", require("./routes/loan"));
@@ -18,5 +24,8 @@ app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
 });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Mini-loan-App Server');
+});
 const dbConnect = require("./config/database");
 dbConnect();
